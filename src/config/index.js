fbe1fda13be024db5ab8ebfff8a6f37e19f1f91a
Vinicius BF <vini91bf@gmail.com>
2020-06-14 17:45:36 -0300

    Setting spotify clientId

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
-    clientId: '',
+    clientId: '4be802c1764640e6bdd07ba03b8d92e0',
     redirectUrl: `${window.location.origin}/authorize`,
     webAPI: 'https://api.spotify.com/v1',
     scopes: [
